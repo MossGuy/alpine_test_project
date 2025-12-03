27986d3aad51b418a294e0324118929086c97435
MossGuy <milanpallas@hotmail.nl>
2025-12-03 16:34:08 +0100

    deleted old code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,4 @@
         <p x-text="message" class="mt-4 text-lg"></p>
     </div>
 </section>
-<section class="mb-3 p-2">
-    <p class="text-blue-700 underline hover:text-cyan-700 transition duration-100"><a href="{{ route('project') }}">Test app</a></p>
-</section>
 @endsection
