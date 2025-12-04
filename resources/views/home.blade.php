e50f03090f287a28a600ed939b3ed9dc242403e3
MossGuy <milanpallas@hotmail.nl>
2025-12-04 13:39:12 +0100

    link to project page added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,7 @@
         <p x-text="message" class="mt-4 text-lg"></p>
     </div>
 </section>
+<section class="mb-3 p-2">
+    <p class="text-blue-700 underline hover:text-cyan-700 transition duration-100"><a href="{{ route('project') }}">Test app</a></p>
+</section>
 @endsection
