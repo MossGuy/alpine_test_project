0d8b1a9b4950bd848aa94f2b982189a2266f8413
MossGuy <milanpallas@hotmail.nl>
2025-12-10 09:08:09 +0100

    added function for api search with watch function

--- resources/js/project.js
+++ resources/js/project.js
@@ -1,5 +1,32 @@
 document.addEventListener("alpine:init", () => {
     Alpine.data('projectData', () => ({
-        
+        query: '',
+        results: [],
+
+        performSearch(query) {
+            if (!query) {
+                this.results = [];
+                return;
+            }
+
+            fetch(`https://hn.algolia.com/api/v1/search?query=${encodeURIComponent(query)}`)
+                .then(res => res.json())
+                .then(data => {
+                    console.log("Fetched results:", data.hits);
+                    this.results = data.hits;
+                })
+                .catch(err => console.error(err));
+        },
+
+        init () {
+            this.$watch('query', (query) => {
+                if (query === '') {
+                    this.results = [];
+                    return;
+                }
+
+                this.performSearch(query);
+            });
+        }
     }));
 });
--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.main')
 
 @section('content')
-<section x-data="projectData">
-    
+<section x-data="projectData" class="mb-3 p-2">
+    <div class="mb-3">
+        <h1>Hacker news API</h1>
+        <input x-model.debounce.200="query" type="text" class="border border-2 rounded p-1" placeholder="Searching for...">
+        <p x-show="query.length" class="mt-2">
+            Your search for
+            <span x-text="query"></span>
+            returned
+            <span x-text="results.length"></span>
+            results
+        </p>
+    </div>
+    <div>
+        <template x-for="result in results" :key="result.objectID">
+            <div class="mb-3 border-b pb-2">
+                <h4 x-text="result.title"></h4>
+                <a :href="result.url" x-text="result.url" class="text-blue-500 hover:text-purple-500 cursor:pointer"></a>
+            </div>
+        </template>
+    </div>
 </section>
 @endsection
